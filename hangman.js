1ed91f9ecbb96f19fa889699dfb05d9d6a277bd9
carlbelle <58005511+carlbelle@users.noreply.github.com>
2023-10-19 23:09:33 -0700

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -13,56 +13,14 @@ let attempts = 6;
 // Function to initialize the game
 function initializeGame() {
     // Display underscores for each letter in the selected word
-    let displayWord = selectedWord.replace(/[a-z]/g, "_");
-    document.getElementById("word-container").textContent = displayWord;
+    displayWordWithUnderscores();
 
     // Display remaining attempts
     document.getElementById("attempts-container").textContent = `Attempts remaining: ${attempts}`;
 }
 
-// Function to make a guess
-function makeGuess() {
-    // Get the guessed letter from the input field
-    let guess = document.getElementById("guess-input").value.toLowerCase();
-
-    // Check if the guessed letter is valid
-    if (guess.match(/[a-z]/) && guess.length === 1) {
-        // Check if the letter has not been guessed before
-        if (!guessedLetters.includes(guess)) {
-            guessedLetters.push(guess);
-
-            // Check if the guessed letter is in the selected word
-            if (selectedWord.includes(guess)) {
-                updateDisplayWord();
-            } else {
-                attempts--;
-            }
-
-            // Display the guessed letters and remaining attempts
-            document.getElementById("guesses-container").textContent = `Guessed letters: ${guessedLetters.join(", ")}`;
-            document.getElementById("attempts-container").textContent = `Attempts remaining: ${attempts}`;
-
-            // Check if the game is won or lost
-            if (isGameWon()) {
-                alert("Congratulations! You won!");
-                resetGame();
-            } else if (attempts === 0) {
-                alert(`Sorry, you lost. The correct word was "${selectedWord}".`);
-                resetGame();
-            }
-        } else {
-            alert("You already guessed that letter. Try another one.");
-        }
-    } else {
-        alert("Please enter a valid single letter.");
-    }
-
-    // Clear the input field
-    document.getElementById("guess-input").value = "";
-}
-
-// Function to update the displayed word with correctly guessed letters
-function updateDisplayWord() {
+// Function to display underscores for each letter in the word
+function displayWordWithUnderscores() {
     let displayWord = "";
     for (let char of selectedWord) {
         if (guessedLetters.includes(char)) {
@@ -74,22 +32,6 @@ function updateDisplayWord() {
     document.getElementById("word-container").textContent = displayWord;
 }
 
-// Function to check if the game is won
-function isGameWon() {
-    return document.getElementById("word-container").textContent === selectedWord;
-}
+// Rest of the code remains the same...
 
-// Function to reset the game
-function resetGame() {
-    // Reset variables
-    guessedLetters = [];
-    attempts = 6;
-    selectedWord = words[Math.floor(Math.random() * words.length)];
-
-    // Reset display
-    initializeGame();
-    document.getElementById("guesses-container").textContent = "";
-}
-
-// Initialize the game when the page loads
-initializeGame();
+// Initialize the
